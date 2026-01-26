06c58eba1b836fc4c847b8bba10cef880303b6f8
Sam Roth <sam-roth@users.noreply.github.com>
2026-01-26 15:52:42 -0800

    Remove unused getter methods from Point class

--- src/Point.hpp
+++ src/Point.hpp
@@ -10,9 +10,6 @@ class Point : public QGraphicsEllipseItem
 public:
     explicit Point(const QPointF &position, const QString &id, QGraphicsItem *parent = nullptr);
 
-    QString getId() const { return _id; }
-    QPointF getPosition() const { return pos(); }
-
     const QString &id() const { return _id; }
 
 private:
